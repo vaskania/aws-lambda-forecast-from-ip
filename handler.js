554e88abe7de3f2a87cb7499c47e40bba2852f32
vaskania <vaskania26@gmail.com>
2022-01-20 11:16:38 +0400

    added axios

--- handler.js
+++ handler.js
@@ -1,26 +1,16 @@
 const axios = require("axios");
-const https = require("https");
 
 const findLocationByIpURL = "http://ip-api.com/json/";
 const weatherApi =
-  process.env.WEATHER_API;
+  process.env.WEATHER_API || "64f266fb3ffa0f2b1da717bf3720157e";
 
 module.exports.weather = async (event) => {
   const ip = event.requestContext.http.sourceIp;
   const url = `${findLocationByIpURL}${ip}`;
   const { data } = await axios(url);
 
-  const promise = new Promise((resolve, reject) => {
-    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${data.city}&appid=${weatherApi}&units=metric`;
-    https.get(weatherUrl, (res) => {
-      if (res.statusCode < 200 || res.statusCode >= 300) {
-        return reject(new Error("statusCode=" + res.statusCode));
-      }
-      res.on("data", (data) => {
-        const response = JSON.parse(data);
-        resolve(response);
-      });
-    });
-  });
-  return promise;
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${data.city}&appid=${weatherApi}&units=metric`;
+  const response = await axios(weatherUrl);
+
+  return response.data;
 };
